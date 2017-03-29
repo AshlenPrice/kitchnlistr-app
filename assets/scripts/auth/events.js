75779110c8295a5932182472f704df014c3e1ee8
ashlenprice <ashlen.price@gmail.com>
2017-03-29 15:27:06 -0400

    Implement Auth for users
    added nav bar, implemented alertify for user facing alerts

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const getFormFields = require(`../../../lib/get-form-fields`)
+const api = require('./api')
+const ui = require('./ui')
+const store = require('../store')
+
+// these functions will run the server requests in the api.js file
+const onSignUp = function (event) {
+  event.preventDefault()
+
+  const data = getFormFields(event.target)
+
+  api.signUp(data)
+    .then(() => {
+      onSignIn(event, data)
+    })
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFail)
+}
+
+const onSignIn = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  console.log(data) // everything look good?
+  api.signIn(data)
+    .then((response) => {
+      console.log(response) // everything look good?
+      store.user = response.user
+      return store.user
+    })
+    .then(ui.signInSuccess)
+    .catch(ui.signInFail)
+  // $('#sign-in')[0].reset();
+}
+
+const onChangePassword = function (event) {
+  event.preventDefault()
+
+  const data = getFormFields(event.target)
+
+  api.changePassword(data)
+    .then(ui.changePwSuccess)
+    .catch(ui.changePwFail)
+}
+
+const onSignOut = function (event) {
+  event.preventDefault()
+
+  api.signOut()
+    .then(() => {
+      delete store.user
+      return store
+    })
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFail)
+}
+
+const addHandlers = () => {
+  $('#sign-up').on('submit', onSignUp)
+  $('#sign-in').on('submit', onSignIn)
+  $('#sign-out').on('click', onSignOut)
+  $('#change-password').on('submit', onChangePassword)
+}
+
+module.exports = {
+  addHandlers
+}
---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,72 @@
+'use strict'
+
+const success = () => {
+  alertify.success('Action Successful')
+}
+
+const signUpSuccess = () => {
+  alertify.success('Account Created!')
+  $('#sign-up')[0].reset()
+  $('#sign-up-prompt').text(' ')// this cancels out the 'account Created msg, need new method '
+  $('#signUpModal').modal('hide')
+}
+
+const signUpFail = () => {
+  alertify.error('There Seems To Be An Error. Try Again!')
+}
+
+const signInFail = () => {
+  alertify.error('There Seems To Be An Error. Try Again!')
+  console.log('failure') // or make it here?
+  $('#sign-in')[0].reset()
+}
+
+const changePwSuccess = () => {
+  alertify.success('Password Successfully Changed!')
+  $('#change-password')[0].reset()
+  $('#changePwModal').modal('hide')
+  // $('#change-pw-prompt').text(' ');
+}
+
+const changePwFail = (data) => {
+  alertify.error("Something's not Right! Try Again!")
+}
+
+const failure = () => {
+  alertify.error('There Seems To Be An Error. Try Again!')
+}
+
+const signInSuccess = () => {
+  alertify.success('Sign-in Succesful!')
+  $('#sign-in')[0].reset()
+  $('#signInModal').modal('hide')
+
+  $('#signUpButton').addClass('hidden')
+  $('#signInButton').addClass('hidden')
+  $('#changePwButton').removeClass('hidden')
+  $('#sign-out').removeClass('hidden')
+  // $('.show').removeClass('hidden');
+}
+
+const signOutSuccess = () => {
+  alertify.success('Success Signed Out!')
+  $('#signUpButton').removeClass('hidden')
+  $('#signInButton').removeClass('hidden')
+  $('#changePwButton').addClass('hidden')
+  $('#sign-out').addClass('hidden')
+  $('#change-pw-prompt').text(' ')
+  $('#sign-up-prompt').text(' ')
+  $('#sign-in-prompt').text(' ')
+}
+
+module.exports = {
+  failure,
+  success,
+  signUpSuccess,
+  signUpFail,
+  signInSuccess,
+  signInFail,
+  changePwFail,
+  changePwSuccess,
+  signOutSuccess
+}
